597edb595924062a204d3c353f2d4ef8329899fb
ErnestH1 <hansonernest7@gmail.com>
2023-03-23 14:45:01 +0300

    updated routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,52 @@
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+// import LoginSignup from './components/auth/login';
+// import Home from './components/pages/home';
+// import Navbar from './components/navbar/navabr';
+// import Todo from './components/pages/todo';
+// import UpdateTodo from './components/pages/todo/updatetodo';
+// import AddTodo from './components/pages/todo/addtodo';
+// import DeleteTodo from './components/pages/todo/deletetodo';
+// import Logout from './components/auth/logout';
+
+// function App() {
+//   const isAuthenticated = false; // Change this to check if the user is authenticated
+
+//   const PrivateRoute = ({ component: Component, ...rest }) => (
+//     <Route {...rest} render={(props) => (
+//       isAuthenticated === true
+//         ? <Component {...props} />
+//         : <Redirect to='/' />
+//     )} />
+//   )
+
+//   return (
+//     <div>
+//       <Router>
+//         <Navbar />
+//         <Switch>
+//           <Route exact path="/" component={LoginSignup} />
+//           <Route path="/signup" component={LoginSignup} />
+//           <PrivateRoute path="/home" component={Home} />
+//           <PrivateRoute path="/todo" component={Todo} />
+//           <PrivateRoute path="/addtodo" component={AddTodo} />
+//           <PrivateRoute path="/update" component={UpdateTodo} />
+//           <PrivateRoute path="/delete" component={DeleteTodo} />
+//           <PrivateRoute path="/logout" component={Logout} />
+//         </Switch>
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+//using the below code in order to bypass authentication(login)
+
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LoginSignup from './components/auth/login';
 import Home from './components/pages/home';
 import Navbar from './components/navbar/navabr';
@@ -7,19 +54,9 @@ import Todo from './components/pages/todo';
 import UpdateTodo from './components/pages/todo/updatetodo';
 import AddTodo from './components/pages/todo/addtodo';
 import DeleteTodo from './components/pages/todo/deletetodo';
-import Logout from './components/auth/logout';
+
 
 function App() {
-  const isAuthenticated = false; // Change this to check if the user is authenticated
-
-  const PrivateRoute = ({ component: Component, ...rest }) => (
-    <Route {...rest} render={(props) => (
-      isAuthenticated === true
-        ? <Component {...props} />
-        : <Redirect to='/' />
-    )} />
-  )
-
   return (
     <div>
       <Router>
@@ -27,12 +64,11 @@ function App() {
         <Switch>
           <Route exact path="/" component={LoginSignup} />
           <Route path="/signup" component={LoginSignup} />
-          <PrivateRoute path="/home" component={Home} />
-          <PrivateRoute path="/todo" component={Todo} />
-          <PrivateRoute path="/addtodo" component={AddTodo} />
-          <PrivateRoute path="/update" component={UpdateTodo} />
-          <PrivateRoute path="/delete" component={DeleteTodo} />
-          <PrivateRoute path="/logout" component={Logout} />
+          <Route path="/home" component={Home} />
+          <Route path="/todo" component={Todo} />
+          <Route path="/addtodo" component={AddTodo} />
+          <Route path="/update" component={UpdateTodo} />
+          <Route path="/delete" component={DeleteTodo} />
         </Switch>
       </Router>
     </div>
--- src/components/navbar/navabr.js
+++ src/components/navbar/navabr.js
@@ -1,35 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container">
+      <div className="container-fluid">
         <Link className="navbar-brand" to="/">Home</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            
-            <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Todo
-          </a>
-          <ul class="dropdown-menu">
-            <Link className="dropdown-item" to="/todo">Todo</Link>
-            <Link className="dropdown-item" to="/addtodo">AddTodo</Link>
-            <Link className="dropdown-item" to="/update">UpdateTodo</Link>
-            <Link className="dropdown-item" to="/delete">DeleteTodo</Link>
-            
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Todo
+              </a>
+              <ul className="dropdown-menu">
+                <li><Link className="dropdown-item" to="/todo">Todo</Link></li>
+                <li><Link className="dropdown-item" to="/addtodo">AddTodo</Link></li>
+                <li><Link className="dropdown-item" to="/update">UpdateTodo</Link></li>
+                <li><Link className="dropdown-item" to="/delete">DeleteTodo</Link></li>
+              </ul>
+            </li>
           </ul>
-
-        </li>
-        <li className="nav-item">
+          <ul className="navbar-nav">
+            <li className="nav-item">
               <Link className="nav-link" to="/logout">Logout</Link>
             </li>
-
           </ul>
         </div>
       </div>
